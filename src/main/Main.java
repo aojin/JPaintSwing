037ea6b4f4f5d9a97f8045d41aed2b8628d65ff5
alexojin <aojin91@gmail.com>
2018-08-02 23:53:54 -0500

    Unified PC & Listener
    Unified the MouseListener and Canvas Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import controller.IJPaintController;
 import controller.JPaintController;
-import model.ShapeList;
 import model.persistence.ApplicationState;
 import view.gui.Gui;
 import view.gui.GuiWindow;
@@ -14,15 +13,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        ShapeList shapeList = new ShapeList(new ArrayList<>());
         PaintCanvas pc = new PaintCanvas();
         IGuiWindow guiWindow = new GuiWindow(pc);
         IUiModule uiModule = new Gui(guiWindow);
         ApplicationState appState = new ApplicationState(uiModule);
+        pc.addGuiObserver(appState);
         IJPaintController controller = new JPaintController(uiModule, appState);
         controller.setup();
-
-        pc.setNewGuiObserver(shapeList, appState);
-        pc.getGraphics2D();
     }
 }
